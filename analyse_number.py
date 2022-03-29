e75875182c314872aafaf4cb6ff9ddeab0276bee
Oliver Jaros <oli.jaros@centrum.sk>
2022-03-29 09:39:22 +0200

    lecture 7_

--- analyse_number.py
+++ analyse_number.py
@@ -12,7 +12,8 @@ def is_even_number(number):
         return False
 
 
-my_number = -5
-print(is_whole_number(my_number))
-print(is_even_number(my_number))
+my_numbers = [-5, 5, 1.2, 5]
+for my_number in my_numbers:
+    print(is_whole_number(my_number))
+    print(is_even_number(my_number))
 ##
