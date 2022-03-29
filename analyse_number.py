ccd5e64fc48f7bba1caddb743d11fadb762b1ed8
Oliver Jaros <oli.jaros@centrum.sk>
2022-03-29 09:06:00 +0200

    uprava 2

--- analyse_number.py
+++ analyse_number.py
@@ -5,5 +5,13 @@ def is_whole_number(number):
         return False
 
 
+def is_even_number(number):
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+
 my_number = -5
 print(is_whole_number(my_number))
+print(is_even_number(my_number))
