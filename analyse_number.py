b5cba34f2f79f1d12a6f421b04255caeb97751d7
Oliver Jaros <oli.jaros@centrum.sk>
2022-03-29 08:56:08 +0200

    lecture 7_uprava

--- analyse_number.py
+++ analyse_number.py
@@ -1,9 +1,9 @@
 def is_whole_number(number):
-    if number % 1 == 0:
+    if number % 1 == 0 and number > 0:
         return True
     else:
         return False
 
 
-my_number = 5
+my_number = -5
 print(is_whole_number(my_number))
